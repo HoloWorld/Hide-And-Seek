5bfa5521748306d3a9effacfc637ea6c8182962d
임병준 <20113316@kookmin.ac.kr>
2016-03-14 18:58:52 +0900

    Unity first commit

---
+++ Unity/Assets/Scripts/EscapeController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+public class EscapeController : MonoBehaviour {
+ 	
+	GameObject[] hide;
+	GameObject selectedObj;
+	Transform player;
+    NavMeshAgent nav;
+	Rigidbody rb;
+   
+    void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		hide = GameObject.FindGameObjectsWithTag ("HideFromFront");
+		
+		// Select one of objects
+		selectedObj = hide[Random.Range(0, hide.Length)];
+		
+        nav = GetComponent <NavMeshAgent> ();
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	void Update() {
+		// Recognize player's position
+		if (player.position.z < 0)
+			hide = GameObject.FindGameObjectsWithTag ("HideFromFront");
+		else if (player.position.x < 0)
+			hide = GameObject.FindGameObjectsWithTag ("HideFromLeft");
+		else if (player.position.x > 20)
+			hide = GameObject.FindGameObjectsWithTag ("HideFromRight");
+
+		
+		// Set destination
+		nav.SetDestination (selectedObj.transform.position);
+		
+		if (!nav.pathPending)
+ 		{
+     		if (nav.remainingDistance <= nav.stoppingDistance)
+     		{
+         		if (!nav.hasPath || nav.velocity.sqrMagnitude == 0f)
+         		{
+             		nav.Stop();
+					Thread.Sleep(100);
+					selectedObj = hide[Random.Range(0, hide.Length)];
+					nav.Resume();
+         		}
+     		}
+		
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 6f;
+	Rigidbody rb;
+	
+	void Awake()
+	{
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	void FixedUpdate() {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+		
+		Move(h, v);
+	
+	}
+	
+	void Move (float h, float v)
+	{
+		Vector3 movement = new Vector3 (h, 0f, v);
+		//Vector3 movement = new Vector3 (0f, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		
+		rb.MovePosition (transform.position + movement);
+	}
+	
+}
