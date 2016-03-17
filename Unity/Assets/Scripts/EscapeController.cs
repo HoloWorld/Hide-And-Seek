84c0c6a5ea0e6cbaba12d222aa661370ad25de29
임병준 <20113316@kookmin.ac.kr>
2016-03-17 14:20:02 +0900

    stop for a while when arrive at the destination

--- Unity/Assets/Scripts/EscapeController.cs
+++ Unity/Assets/Scripts/EscapeController.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Threading;
+
+
 public class EscapeController : MonoBehaviour {
  	
 	GameObject[] hide;
 	GameObject selectedObj;
 	Transform player;
     NavMeshAgent nav;
-	Rigidbody rb;
    
     void Awake () {
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
@@ -17,7 +18,6 @@ public class EscapeController : MonoBehaviour {
 		selectedObj = hide[Random.Range(0, hide.Length)];
 		
         nav = GetComponent <NavMeshAgent> ();
-		rb = GetComponent <Rigidbody> ();
 	}
 	
 	void Update() {
@@ -28,24 +28,40 @@ public class EscapeController : MonoBehaviour {
 			hide = GameObject.FindGameObjectsWithTag ("HideFromLeft");
 		else if (player.position.x > 20)
 			hide = GameObject.FindGameObjectsWithTag ("HideFromRight");
-
 		
 		// Set destination
 		nav.SetDestination (selectedObj.transform.position);
-		
-		if (!nav.pathPending)
- 		{
-     		if (nav.remainingDistance <= nav.stoppingDistance)
-     		{
-         		if (!nav.hasPath || nav.velocity.sqrMagnitude == 0f)
-         		{
-             		nav.Stop();
-					Thread.Sleep(100);
-					selectedObj = hide[Random.Range(0, hide.Length)];
-					nav.Resume();
-         		}
-     		}
-		
+
+		// Is it arrived to destination?
+		if (IsArrived ()) {
+			StartCoroutine (StopEscape ());
+			selectedObj = hide [Random.Range (0, hide.Length)];
+		} else {	// On the way to destination
+
+			// Escape player or keep going
+			// 1) Escape player
+			// Set new destination most far from player
+			// 2) Keep going
+			// Just go to first destination and sleep for a while
+
 		}
+	}
+
+	bool IsArrived() {
+		if (!nav.pathPending)
+			if (nav.remainingDistance <= nav.stoppingDistance)
+				if (!nav.hasPath || nav.velocity.sqrMagnitude == 0f)
+					return true;
+				
+		return false;
+	}
+		
+	// Stop escaping for a while
+	IEnumerator StopEscape() {
+		nav.Stop();
+
+		yield return new WaitForSeconds(3);
+
+		nav.Resume();
 	}
 }
