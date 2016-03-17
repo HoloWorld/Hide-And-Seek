cdc2799a871f0f744948d7110bf8970283b7b652
임병준 <20113316@kookmin.ac.kr>
2016-03-17 14:03:22 +0900

    Finding obastcles

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	public float speed = 6f;
 	Rigidbody rb;
-	
+	NavMeshAgent nav;
+	Transform target;
+
 	void Awake()
 	{
 		rb = GetComponent <Rigidbody> ();
+		nav = GetComponent <NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Target").transform;
 	}
 	
 	void FixedUpdate() {
@@ -15,16 +19,40 @@ public class PlayerMovement : MonoBehaviour {
 		float v = Input.GetAxisRaw("Vertical");
 		
 		Move(h, v);
-	
+
+		if(IsObstacle ())
+			Debug.Log ("Target is invisible");
+		else
+			Debug.Log ("Target is visible");
+
+
 	}
-	
+
+	// Player movement controll
 	void Move (float h, float v)
 	{
 		Vector3 movement = new Vector3 (h, 0f, v);
-		//Vector3 movement = new Vector3 (0f, 0f, v);
+
 		movement = movement.normalized * speed * Time.deltaTime;
 		
 		rb.MovePosition (transform.position + movement);
 	}
-	
+
+	// if there is a obstacle between player and target, return true
+	// else return false
+	bool IsObstacle()
+	{
+		bool isOb = true;
+
+		// set nav
+		nav.SetDestination (target.position);
+
+		// test for obstacle between target and player
+		isOb = nav.remainingDistance > Vector3.Distance (this.transform.position, target.position) + 0.1;
+
+		// stop nav because Player's nav is just for finding path.
+		nav.Stop ();
+
+		return isOb;
+	}
 }
