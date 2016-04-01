a295d1d1ca0ab91c6734a7911fa4859682325bbb
임병준 <20113316@kookmin.ac.kr>
2016-04-01 15:22:02 +0900

    Select Map merge

---
+++ Kinect_Holo_ver1.0for2.0/Assets/Script/UI/SelectMap/OptionActions/GoToScene_OptionAction.cs
@@ -0,0 +1,30 @@
+﻿//allows us to go from one scene to another
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+[RequireComponent (typeof (OptionScript))]
+public class GoToScene_OptionAction : OptionAction_WithButton {
+
+	public string SceneName = "";
+
+	// Update is called once per frame
+	void Update () 
+	{
+		try
+		{
+			if (gameObject.GetComponent<OptionScript>().Focus  && Input.GetButtonDown(Button) )
+			{
+				SceneManager.LoadScene(SceneName);
+
+				GetComponent<AudioSource>().GetComponent<AudioSource>().PlayOneShot(ActionAudio); 
+				//this audio will only be heard if this GameObject has a Object.DontDestroyOnLoad on it.
+			}
+		}
+		catch(UnityException)
+		{
+			print ("your 'Button' string value is not correct. Go to Edit -> Project Settings -> Input, then change the value to a correct one");
+		}
+	}
+}
--- SelectMap/Assets/3D Menu System/Scripts/RotationalMenu_OptionScript.cs
+++
@@ -1,9 +0,0 @@
-﻿//this is the Grid_OptionScript and it inherits from OptionScript
-
-using UnityEngine;
-using System.Collections;
-
-public class RotationalMenu_OptionScript : OptionScript {
-
-
-}
